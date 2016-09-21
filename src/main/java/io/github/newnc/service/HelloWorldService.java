1286adf96a345f0b49f06f6b4226d84e93c37df1
Guilherme José de Carvalho Gois <guijocargo@live.com>
2016-09-21 08:41:41 -0300

    mvn spring-boot:run

---
+++ src/main/java/io/github/newnc/service/HelloWorldService.java
@@ -0,0 +1,15 @@
+package io.github.newnc.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldService {
+
+  @RequestMapping("/")
+  public String home() {
+    return "We are up and running! "
+      + "You ever watched Star Wars today? "
+      + "May be the force be with you!";
+  }
+}
